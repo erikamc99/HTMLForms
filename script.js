728c342db1e5a1e2cb7915bdf411811aef5ed737
Erica <you@example.com>
2025-01-11 22:52:56 +0100

    final version

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-/* Este es el primer código que hice, con el que esta sin comentar todos los campos actúan como required.
-document.getElementById('submit').addEventListener('click', (event) => {
-    event.preventDefault();
-
-    let getValue = (id) => document.getElementById(id)?.value;
-    let getResultValue = (name) => document.querySelector(`input[name="${name}"]:checked`)?.value;
-    let getResultValues = (name) => 
-        Array.from(document.querySelectorAll(`input[name="${name}"]:checked`)).map(el => el.value);
-
-    let result = {
-        uname: getValue('uname'),
-        email: getValue('uemail'),
-        age: getValue('uage'),
-        description: getValue('description'),
-        recommend: getResultValue('recommend'),
-        langframe: getResultValues('langframe'),
-        comments: getValue('comments')
-    };
-
-    console.log(JSON.stringify(result, null, 2));
-
-    document.body.innerHTML = '<div class="form-submitted"><p>Formulario enviado. ¡Muchas gracias!</p></div>';
-});
-*/
-
 document.querySelector('form').addEventListener('submit', (event) => {
     event.preventDefault();
 
